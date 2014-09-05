893870f94fe175981ac2c640614eaf88e7382d5d
zigzackattack <zackbabtkis@gmail.com>
2014-09-04 23:06:00 -0700

    Added app src file with Playlitics module declaration wrapped in a nice cozy closure

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/**
+ * Module:Playlitics
+ *
+ * -- An application for building awesome Spotify playlists
+ *
+ * @author  Zachary Babtkis <zackbabtkis@gmail.com>
+ * @license WTFPL
+ */
+
+;(function( ng ) {
+
+	"use strict";
+
+	// Register 'Playlitics' main app module
+	var app = ng.app('Playlitics', []);
+
+}).call( this, angular )
+
