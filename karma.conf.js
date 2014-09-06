b4b2e91c994e5f936f93354f0081001c586bbccd
zigzackattack <zackbabtkis@gmail.com>
2014-09-06 11:40:22 -0700

    Added angular and angular-mock to karma files

--- karma.conf.js
+++ karma.conf.js
@@ -2,8 +2,10 @@ module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
 		files: [
+			'components/angular/angular.js',
+			'components/angular-mocks/angular-mocks.js',
+			'src/**',
 			'test/unit/spec.js',
-			'index.html'
 		]
   });
 };
