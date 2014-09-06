71870afcfa05eb7b8f15b386443869de3f729cf3
zigzackattack <zackbabtkis@gmail.com>
2014-09-06 15:36:50 -0700

    Implemented and added tests for Playlitics.Store module persisting, fetching and serializing data

--- src/services/store.js
+++ src/services/store.js
@@ -1,19 +1,44 @@
+/**
+ * Playlitics.Store
+ * localStorage DataStore service
+ */
 ;(function ( ng ) {
 
 	"use strict";
 
-	function StoreNotFound () {
-		ReferenceError.apply( this, arguments );
+	/**
+	 * StoreNotFoundError
+	 * Thrown if a requested data store has no data
+	 * 
+	 * @inherits ReferenceError
+	 */
+	function StoreNotFoundError ( msg ) {
+		this.name    = 'StoreNotFound';
+		this.message = msg;
+		this.stack   = (new Error).stack;
 	}
 
-	StoreNotFound.prototype = Object.create(ReferenceError.prototype);
+	StoreNotFoundError.prototype = new ReferenceError;
 
-	ng.app('Playlitics.Store', [])
+	// Define new submodule for service
+	ng.module('Playlitics.Store', [])
 
 		.service( 'Store', function( $window, $q) {
 
-			var Store = $window.localStorage || $window.webkitLocalStorage || $window.mozLocalStorage;
+			// provate handle to vendor specific localStorage
+			var Store = $window.localStorage || $window.webkitLocalStorage || $window.mozLocalStorage || {};
 
+			// publically accessible handle to localStorage impelemntation 
+			this._storage = Store;
+
+			/**
+			 * #fetch()
+			 * Fetch a stored value by storeName
+			 * 
+			 * @param { String } - storeName - key in localStorage to load value from
+			 * @return { Promise } - resolves with stored data if available or rejects
+			 *   with error of store doesn't exist
+			 */
 			this.fetch = function( storeName ) {
 				var d = $q.defer();
 
@@ -21,9 +46,9 @@
 				var store = Store[ storeName ];
 
 				// If store hasn't been created, reject promise with StoreNotFound error
-				if ( typeof store === 'undefined' ) 
-					d.reject(
-						new StoreNotFoundError('Error: No store exists with name ' + store)
+				if ( typeof store === 'undefined' ) { 
+					d.reject( 
+						new StoreNotFoundError( 'Error: No store exists with name ' + storeName )
 					);
 				} else {
 					d.resolve( this.deserialize( store ) );
@@ -32,11 +57,46 @@
 				// Return a promise for rejected or resolved store request
 				return d.promise;
 			};
-		});
 
-		this.persist = function ( storeName, data ) {
-			Store[ storeName ] = this.serialize( data );
-		};
+			/**
+			 * #deserialize( data );
+			 * Covert localStorage safe string back to an object
+			 * 
+			 * @param  { String } - data - string to be converted to object
+			 * @return { Object } - JS object deserialized from string
+			 */
+			this.deserialize = function ( data ) {
+				return JSON.parse(data);
+			};
+
+			/**
+			 * #serialize( data );
+			 * Covert JS object to localStorage safe string
+			 * 
+			 * @param  { Object } - data - JS object to serialize
+			 * @return { String } - serialized string from object
+			 */
+			this.serialize = function ( data ) {
+				return JSON.stringify(data);
+			};
+
+			/**
+			 * #persist( storeName, data );
+			 * Save an object to localStorage
+			 *
+			 * @param  { String } - storeName - key to use to retrieve data from store
+			 * @return { Promise } - for interface compatibility
+			 */
+			this.persist = function ( storeName, data ) {
+				var d = $q.defer();
+
+				d.resolve(
+					Store[ storeName ] = this.serialize( data )
+				);
+
+				return d.promise;
+			};
+		});
 
 }).call( this, angular );
 
--- test/unit/spec.js
+++ test/unit/spec.js
@@ -1,3 +1,95 @@
-describe("Playlist", function () {
- it("Calculates epsilon of each song's coolness");
+describe("Store", function() {
+	
+	beforeEach(function() {
+		module("Playlitics.Store");
+	});
+
+	beforeEach(inject(function($window, $rootScope, Store) {
+		this.$window = $window;
+		this.Store = Store;
+		this.$rootScope = $rootScope;
+	}));
+
+	describe("when fetching a stored value", function() {
+
+		it("resolves a StoreNotFoundError if no store exists", function() {
+
+			// setup
+			var response;
+
+
+			// run
+			runs(function() {
+				this.Store.fetch('tests')
+					.catch(function(data) {
+						response = data;
+					});
+
+				// Allow promises to be resolved
+				this.$rootScope.$digest();
+			});
+
+			// assert
+			runs(function() {
+				expect(response.message).toEqual('Error: No store exists with name tests');
+			});
+		});
+
+		it("resolves a deserialized object", function() {
+
+			// setup
+			var greeting = JSON.stringify({greeting: "hello"})
+				, response;
+
+			this.Store._storage['greeting'] = greeting;
+
+			// run
+			runs(function() {
+				this.Store.fetch('greeting')
+					.then(function(data) {
+						response = data;
+					})
+					.catch(function( error ) {
+						console.debug( error );
+					});
+
+				// Allow promises to be resolved
+				this.$rootScope.$digest();
+			});
+
+			// assert
+			runs(function() {
+				expect(response.greeting).toEqual('hello');
+			});
+		});
+
+	});
+
+	describe("when persisting a stored value", function() {
+
+		it("serializes the stored object", function() {
+			// setup 
+			var greeting = {greeting: "hey there!"}; 
+
+			spyOn(this.Store, 'serialize').andCallThrough();;
+
+			// run
+			this.Store.persist('greeting', greeting);
+
+			// assert
+			expect(this.Store.serialize).toHaveBeenCalledWith(greeting)
+		});
+
+		it("stores serialized object", function() {
+			// setup 
+			var playlist = {playlist: ["feets too big"]}; 
+
+			// run
+			this.Store.persist('playlist', playlist);
+
+			// assert
+			expect(this.Store._storage.playlist).toEqual(JSON.stringify(playlist));
+		});
+	});
+	
 });
