f496cdec174c97a1799c436ae118b84ce4d40645
zigzackattack <zackbabtkis@gmail.com>
2014-09-06 17:16:33 -0700

    Added Spotify service and unit tests for querying songs

--- src/services/playlitics.data.spotify.js
+++ src/services/playlitics.data.spotify.js
@@ -5,6 +5,42 @@
 
 ;(function(ng) {
 
-	angular.module('Playlitics.Data')
+	"use strict";
+
+	angular.module( 'Playlitics.Data' )
+
+		.service( 'Spotify', function( $http ) {
+
+			// Default settings for spotify query building
+			var settings = {};
+
+			// Spotify track query endpoint
+			settings.API_ENDPOINT            = "http://ws.spotify.com/search/1/track.json";
+			
+			// JSONP window callback name
+			settings.JSONP_RESPONSE_CALLBACK = "JSON_CALLBACK"
+
+			/**
+			 * #search( q )
+			 * Create XHR query for matching songs in spotify library
+			 *
+			 * @param { String } - q search term
+			 * @return { Promise } - promise of search response
+			 */
+			this.search = function( q ) {
+				return $http.jsonp(settings.API_ENDPOINT + "?q=" + this.safeQuery(q) + "&callback=" + settings.JSONP_RESPONSE_CALLBACK);
+			};
+
+			/**
+			 * #safeQuery( q );
+			 * Convert regular string to URI safe string
+			 * 
+			 * @param { String } - q - query string
+			 * @return { String } URI safe string
+			 */
+			this.safeQuery = function( q ) {
+				return encodeURI( q );
+			};
+		});
 	
-}).call(this, angular);
+}).call( this, angular );
--- test/unit/spec.js
+++ test/unit/spec.js
@@ -1,4 +1,4 @@
-describe("Store", function() {
+describe("Playlitics.Data.Store", function() {
 	
 	beforeEach(function() {
 		module("Playlitics.Data");
@@ -9,6 +9,8 @@ describe("Store", function() {
 		this.Store = Store;
 		this.$rootScope = $rootScope;
 	}));
+
+
 
 	describe("when fetching a stored value", function() {
 
@@ -92,4 +94,63 @@ describe("Store", function() {
 		});
 	});
 	
+});
+
+describe("Playlitics.Data.Spotify", function() {
+
+	beforeEach(function() {
+		module("Playlitics.Data");
+	});
+
+	beforeEach(inject(function( Spotify, $httpBackend ) {
+		var songs = ["hello", "goodbye"];
+
+		$httpBackend.expectJSONP(/http\:\/\/ws.spotify.com.*?/g)
+			.respond(201, songs);
+
+		this.$httpBackend = $httpBackend;
+		this.Spotify = Spotify;
+	}));
+
+	describe("searching spotify songs", function() {
+
+		it("resolves returned promise with an array in data property", function() {
+
+			// setup
+			var response;
+
+			// run
+			runs(function() {
+				this.Spotify.search("jack")
+					.then(function( data ) {
+						response = data;
+					})
+					.catch(function(message) {
+						console.debug( message );
+					});
+
+				this.$httpBackend.flush()
+			});
+
+			waitsFor(function() {
+				return response;
+			}, "search yields results", 100);
+
+			// assert
+			runs(function() {
+				expect(response.data instanceof Array).toBe(true);
+			});
+
+		});
+
+	});
+
+	describe("converting query to safe URI string", function() {
+		it("converts query string to URI safe string", function() {
+			
+			// assert
+			expect(this.Spotify.safeQuery('Jack Johnson')).toBe('Jack%20Johnson');
+		});
+	});
+
 });
