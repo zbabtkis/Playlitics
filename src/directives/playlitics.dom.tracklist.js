6dd391bc6fc2b89b7a587a86919df0b46f37b714
zigzackattack <zackbabtkis@gmail.com>
2014-09-08 04:44:41 -0700

    broadcast message to rootscope that track has been selected on tracklist

--- src/directives/playlitics.dom.tracklist.js
+++ src/directives/playlitics.dom.tracklist.js
@@ -1,10 +1,17 @@
+/**
+ * trackList
+ * A directive for listing songs
+ * 
+ * @package Playlitics.DOM
+ * @class   Directive
+ */
 ;(function( ng ) {
 
 	"use strict";
 
 	ng.module("Playlitics.DOM")
 		
-		.directive( "trackList", function( $parse ) {
+		.directive( "trackList", function( $parse, $rootScope ) {
 			return {
 				restrict: 'EA',
 				
@@ -13,7 +20,10 @@
 				link: function( scope, el, attr ) {
 					var trackSelectCallback = $parse(attr['onTrackSelect'])(scope);
 
-					scope.trackSelect = trackSelectCallback;
+					scope.trackSelect = function( track ) {
+						$rootScope.$broadcast("trackSelected", track);
+						trackSelectCallback( track );
+					};
 				}
 			};
 		});
