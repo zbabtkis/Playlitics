0751e2136c0a7698c49996dd63ac2a8c2499f822
zigzackattack <zackbabtkis@gmail.com>
2014-09-08 04:35:29 -0700

    Added module for Playlitics filters

---
+++ src/filters/playlitics.filters.js
@@ -0,0 +1,12 @@
+/**
+ * @package Playlitics.Filters
+ * Module declaration for Playlitics.Filters
+ */
+
+;(function( ng ) {
+
+	"use strict";
+
+	ng.module("Playlitics.Filters", []);
+	
+}).call(this, angular);
