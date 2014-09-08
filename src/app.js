032c49e302eb59fd8066a634089ff614d8cf97af
zigzackattack <zackbabtkis@gmail.com>
2014-09-07 18:27:44 -0700

    Added route to app config for playlist ui view

--- src/app.js
+++ src/app.js
@@ -13,8 +13,18 @@
 
 	// Register 'Playlitics' main app module
 	var app = ng.module('Playlitics', [
+		'ui.router',
 		'Playlitics.Data',
 		'Playlitics.DOM']);
+
+	app.config(function($stateProvider) {
+		$stateProvider
+			.state('playlist', {
+				url: '/:permalink',
+				templateUrl: 'views/playlist.html',
+				controller: 'PlaylistCtrl'
+			});
+	});
 
 }).call( this, angular )
 
