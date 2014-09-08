0c7604ba0f49f7a34ebb210b0244f548b723951a
zigzackattack <zackbabtkis@gmail.com>
2014-09-08 04:41:07 -0700

    Fixed app routing so it defaults to home route with message about creating new playlist

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 /**
- * Module:Playlitics
+ * Playlitics
  *
  * -- An application for building awesome Spotify playlists
  *
+ * @package Playlitics
  * @author  Zachary Babtkis <zackbabtkis@gmail.com>
  * @license WTFPL
  */
@@ -14,25 +15,36 @@
 	// Register 'Playlitics' main app module
 	var app = ng.module('Playlitics', [
 		'ui.router',
+		'ngRepeatReorder',
 		'Playlitics.Data',
-		'Playlitics.DOM']);
+		'Playlitics.DOM',
+		'Playlitics.Filters']);
 
-	app.config(function($stateProvider) {
+	app.config(function($stateProvider, $urlRouterProvider) {
 		$stateProvider
+
+			// UI router's way of saying home...
 			.state('playlists', {
-				url: '/playlists',
 				abstract: true,
-				template: '<ui-view />'
+				template: '<ui-view />'	
 			})
+
+			// Define what the home route's index page should look like
 			.state('playlists.index', {
 				url: '/',
 				templateUrl: 'views/selectplaylist.html'
 			})
+
+			// The route for viewing a playlist
 			.state('playlists.details', {
 				url: '/:permalink',
 				templateUrl: 'views/playlist.html',
 				controller: 'PlaylistCtrl'
 			});
+
+		// If not on a playlist route, go to playlist index
+		$urlRouterProvider
+			.otherwise('/')
 	});
 
 }).call( this, angular )
